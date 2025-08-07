75ed93a7f11149d7ff175ac8f25cc30814b99deb
Banti4750 <bantikumar6203818460@gmail.com>
2025-08-08 03:17:04 +0530

    refactor: remove unused component imports from App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,4 @@
 import './App.css'
-import AuthPage from './components/AuthPage'
-import Binomomakes from './components/Binomomakes'
-import ChartComponent from './components/ChartComponent'
-import Deposit from './components/Deposit'
-import Footer from './components/Footer'
-import HeroSection from './components/HeroSection'
-import Joinsuccessfultraders from './components/Joinsuccessfultraders'
-import Peopleworldwide from './components/Peopleworldwide'
 
 function App() {
   return (
